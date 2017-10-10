3a9df16a085a9de784f429fc396b7163bc852d86
Rob Smith <rsmith@vsapartners.com>
2017-10-10 15:31:16 -0500

    add some routing stuff

---
+++ src/app/mainview.component.ts
@@ -0,0 +1,8 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'mainview',
+  templateUrl: './mainview.component.html',
+  // styleUrls: ['./mainview.component.scss']
+})
+export class MainviewComponent { }
