53ed6d8124a78975c7c9dbd9fcbf773e8edcaebb
Rob Smith <rsmith@vsapartners.com>
2017-10-27 15:21:22 -0500

    calc on buy market order form

--- src/app/orderform.component.ts
+++ src/app/orderform.component.ts
@@ -1,28 +1,49 @@
 import { Component, Input } from '@angular/core';
 import { DecimalPipe } from '@angular/common';
 
+import { Currentprice } from './currentprice';
+import { CurrentpriceService } from './currentprice.service';
+
 @Component({
   selector: 'orderform',
   templateUrl: './orderform.component.html',
+  providers: [CurrentpriceService]
   // styleUrls: ['./open-orders.component.scss']
 })
 export class OrderformComponent {
   title = 'Order Form';
+  currentprice: Currentprice;
+
 
   @Input() public symbols:string[];
 
-  constructor(private decimalPipe:DecimalPipe) { }
+  constructor(private currentpriceService: CurrentpriceService,private decimalPipe:DecimalPipe) { }
 
   formatNumber(num:string, symbol:string): string {
     const format = symbol !== "USD" ? "1.8-8" : "1.2-2";
     return this.decimalPipe.transform(num,format);
   }
 
+
+  ngOnInit() {
+    this.getCurrentprice();
+  }
+
+  ngOnChanges() {
+    this.getCurrentprice();
+  }
+
+  getCurrentprice(): void {
+    this.currentpriceService.getCurrentprice(this.symbols).then(currentprice => {
+      this.currentprice = currentprice[0];
+    })
+  }
+
   totalPrice = 0;
 
   calcPrice(event: any) { // without type info
     var enteredValue = event.target.value;
-    var currPrice = 100;
+    var currPrice = parseFloat(this.currentprice.last);
     this.totalPrice = enteredValue * currPrice;
   }
 }
