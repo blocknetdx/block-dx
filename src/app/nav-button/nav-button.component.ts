71cde76c4d84bacabec18f80d2e5689806289ca1
Corey Roach <caroach@gmail.com>
2017-11-06 13:49:32 -0600

    Added custom grid to trading panels.

--- src/app/nav-button/nav-button.component.ts
+++ src/app/nav-button/nav-button.component.ts
@@ -6,7 +6,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./nav-button.component.scss']
 })
 export class NavButtonComponent {
-  @Input() public view: HTMLDivElement;
+  @Input() public views: HTMLDivElement[];
 
   @Output('onActive')
   public activeEmitter: EventEmitter<NavButtonComponent> = new EventEmitter();
@@ -14,7 +14,10 @@ export class NavButtonComponent {
   private _active: boolean;
   public get active(): boolean { return this._active; }
   public set active(val: boolean) {
-    this.view.style.display = val ? '' : 'none';
+    this.views.forEach((view) => {
+      view.style.display = val ? '' : 'none';
+    });
+
     this._active = val;
   }
 
