eb7dbf7fdedb682451072cb44bb9f64a492d4365
Corey Roach <caroach@gmail.com>
2017-11-27 14:28:53 -0600

    All components now using card component.

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'bn-card',
+  encapsulation: ViewEncapsulation.None,
   styleUrls: ['./card.component.scss'],
   template: `
-    <div class="bn-card">
-      <div class="bn-card__title">
+    <div class="bn-card {{cardClass}}">
+      <div class="bn-card__title {{cardTitleClass}}">
         <ng-content select="bn-card-title"></ng-content>
       </div>
       <div class="bn-card__body">
@@ -15,6 +16,8 @@ import { Component, OnInit } from '@angular/core';
   `
 })
 export class CardComponent implements OnInit {
+  @Input() cardClass: string;
+  @Input() cardTitleClass: string;
 
   constructor() { }
 
