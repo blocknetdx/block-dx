115bf4127a67cba32afa0ad6a9be58d442d5a249
Corey Roach <caroach@gmail.com>
2017-11-30 11:03:02 -0600

    #49 Zoom in / out controls missing on Depth chart

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'bn-card',
   encapsulation: ViewEncapsulation.None,
   styleUrls: ['./card.component.scss'],
   template: `
-    <div class="bn-card {{cardClass}}">
+    <div #card class="bn-card {{cardClass}}">
       <div class="bn-card__title {{cardTitleClass}}">
         <ng-content select="bn-card-title"></ng-content>
+        <a class="fullscreen" *ngIf="allowFullscreen"
+          (click)="goFullscreen()">
+          <i class="material-icons">zoom_out_map</i>
+        </a>
       </div>
       <div class="bn-card__body">
         <ng-content select="bn-card-body"></ng-content>
@@ -18,10 +22,27 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 export class CardComponent implements OnInit {
   @Input() cardClass: string;
   @Input() cardTitleClass: string;
+  @Input() allowFullscreen: boolean = true;
+
+  @ViewChild('card') public card: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  goFullscreen() {
+    if (!this.allowFullscreen) return;
+
+    if (this.card.nativeElement.requestFullscreen) {
+      this.card.nativeElement.requestFullscreen();
+    } else if (this.card.nativeElement.msRequestFullscreen) {
+      this.card.nativeElement.msRequestFullscreen();
+    } else if (this.card.nativeElement.mozRequestFullScreen) {
+      this.card.nativeElement.mozRequestFullScreen();
+    } else if (this.card.nativeElement.webkitRequestFullscreen) {
+      this.card.nativeElement.webkitRequestFullscreen();
+    }
   }
 
 }
