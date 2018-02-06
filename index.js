7ab83bce45834d5a0c26614a5a576ce209eb74fa
rBurgett <ryan@burgettweb.net>
2018-02-06 16:56:53 -0500

    Added electron runtime and configured build process

---
+++ index.js
@@ -0,0 +1,115 @@
+const co = require('co');
+const electron = require('electron');
+const express = require('express');
+const isDev = require('electron-is-dev');
+const path = require('path');
+const portscanner = require('portscanner');
+
+// General Error Handler
+const handlError = err => {
+  console.error(err);
+};
+
+// Handle any uncaught exceptions
+process.on('uncaughtException', err => {
+  handlError(err);
+});
+
+const { app, BrowserWindow, Menu } = electron;
+
+// Only allow one application instance to be open at a time
+const isSecondInstance = app.makeSingleInstance(() => {});
+if(isSecondInstance) app.quit();
+
+// Run the application within a generator for async flow control
+co(function*() {
+  try {
+
+    const localhost = 'http://127.0.0.1';
+
+    let port;
+
+    // In development use the live ng server. In production serve the built files
+    if(isDev) {
+      port = 4200;
+    } else {
+      const ports = [];
+      for(let i = 0; i < 1000; i++) {
+        ports.push(51236 + i);
+      }
+      port = yield portscanner.findAPortNotInUse(ports, localhost);
+      yield new Promise(resolve => {
+        express()
+            .use(express.static(path.join(__dirname, 'dist')))
+            .listen(port, () => {
+              resolve();
+            });
+      });
+    }
+
+    const serverLocation =  `${localhost}:${port}`;
+
+    app.on('ready', () => {
+
+      const { width, height } = electron.screen.getPrimaryDisplay().workAreaSize;
+
+      const appWindow = new BrowserWindow({
+        show: false,
+        width: width - 100,
+        height: height - 100
+      });
+
+      appWindow.loadURL(serverLocation);
+
+      appWindow.once('ready-to-show', () => {
+        appWindow.show();
+      });
+
+      const menuTemplate = [];
+
+      // File Menu
+      menuTemplate.push({
+        label: 'File',
+        submenu: [
+          { role: 'quit' }
+        ]
+      });
+
+      // Edit Menu
+      menuTemplate.push({
+        label: 'Edit',
+        submenu: [
+          { role: 'undo' },
+          { role: 'redo' },
+          { type: 'separator' },
+          { role: 'cut' },
+          { role: 'copy' },
+          { role: 'paste' },
+          { role: 'selectall' }
+        ]
+      });
+
+      // Window Menu
+      if(isDev) {
+        menuTemplate.push({
+          label: 'Window',
+          submenu: [
+            { label: 'Show Dev Tools', role: 'toggledevtools' }
+          ]
+        });
+      }
+
+      const appMenu = Menu.buildFromTemplate(menuTemplate);
+      Menu.setApplicationMenu(appMenu);
+
+    });
+
+    // Properly close the application
+    app.on('window-all-closed', () => {
+      app.quit();
+    });
+
+  } catch(err) {
+    handlError(err);
+  }
+});
