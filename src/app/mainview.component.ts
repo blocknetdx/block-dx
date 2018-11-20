21ba57edef05a644665c55ee8947af7ff415cb73
rBurgett <ryan@burgettweb.net>
2018-11-20 13:08:25 -0500

    Updated order form to properly display when alternate pricing enabled

--- src/app/mainview.component.ts
+++ src/app/mainview.component.ts
@@ -1,7 +1,8 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, NgZone, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { AppService } from './app.service';
+import { PricingService } from './pricing.service';
 import { OrderbookService } from './orderbook.service';
 import { OrderbookComponent } from './orderbook.component';
 
@@ -10,18 +11,21 @@ import { OrderbookComponent } from './orderbook.component';
   templateUrl: './mainview.component.html',
   styleUrls: ['./mainview.component.scss']
 })
-export class MainviewComponent {
+export class MainviewComponent implements OnInit {
   @ViewChild('orderbook')
   public orderbook: OrderbookComponent;
 
   public orderCardIndex = 0; // Temporary FIXME
   public decimalOptions: any[];
   public initialDecimalIdx: number;
+  public pricingEnabled = false;
 
   constructor(
     private route: ActivatedRoute,
     private appService: AppService,
-    private orderbookService: OrderbookService
+    private orderbookService: OrderbookService,
+    private pricingService: PricingService,
+    private zone: NgZone
   ) {
     this.decimalOptions = [
       {value: '8', viewValue:'8 decimals'},
@@ -31,6 +35,14 @@ export class MainviewComponent {
     ];
     const initialPriceDecimal = localStorage.getItem('priceDecimal') || '6';
     this.initialDecimalIdx = this.decimalOptions.findIndex(({ value }) => value === initialPriceDecimal);
+  }
+
+  ngOnInit() {
+    this.pricingService.getPricingEnabled().subscribe(enabled => {
+      this.zone.run(() => {
+        this.pricingEnabled = enabled;
+      });
+    });
   }
 
   onNavChange(list) {
