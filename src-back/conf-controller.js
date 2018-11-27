5a93c9674ee9e1e1099f5b37ac62e4150014debc
rBurgett <ryan@burgettweb.net>
2018-11-27 13:51:41 -0500

    Added local config files to fall back on when unable to connect to GitHub

--- src-back/conf-controller.js
+++ src-back/conf-controller.js
@@ -16,14 +16,42 @@ module.exports.getManifest = async function() {
   return JSON.parse(text);
 };
 
+const atATime = (max, promiseFuncs) => new Promise((resolve, reject) => {
+  promiseFuncs = [...promiseFuncs];
+  const resultsObj = {};
+  const promises = [];
+  for(let i = 0; i < max; i++) {
+    promises.push((async function() {
+      while(promiseFuncs.length > 0) {
+        const idx = promiseFuncs.length - 1;
+        const func = promiseFuncs.pop();
+        const res = await func();
+        resultsObj[idx] = res;
+      }
+    })());
+  }
+  Promise
+    .all(promises)
+    .then(() => {
+      resolve(Object
+        .keys(resultsObj)
+        .sort((a, b) => {
+          a = Number(a);
+          b = Number(b);
+          return a === b ? 0 : a > b ? 1 : -1;
+        })
+        .map(key => resultsObj[key])
+      );
+    })
+    .catch(reject);
+});
+
 module.exports.getWalletConfs = async function() {
   const { body } = await request
     .get(`${apiBase}/contents/wallet-confs`);
   const files = body
     .filter(f => f.type === 'file');
-  const data = await Promise.all(
-    files.map(f => request.get(f.download_url))
-  );
+  const data = await atATime(20, files.map(f => () => request.get(f.download_url)));
   return data
     .map((d, i) => {
       return [files[i].name, d.text];
@@ -38,9 +66,7 @@ module.exports.getXbridgeConfs = async function() {
     .get(`${apiBase}/contents/xbridge-confs`);
   const files = body
     .filter(f => f.type === 'file');
-  const data = await Promise.all(
-    files.map(f => request.get(f.download_url))
-  );
+  const data = await atATime(20, files.map(f => () => request.get(f.download_url)));
   return data
     .map((d, i) => {
       return [files[i].name, d.text];
