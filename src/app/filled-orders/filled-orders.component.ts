f851792b09a929fabcddeac6c32db025350b1da5
hanniabu <hanniabu@gmail.com>
2019-01-11 03:06:55 -0500

    simple status, status hover, order filter

--- src/app/filled-orders/filled-orders.component.ts
+++ src/app/filled-orders/filled-orders.component.ts
@@ -54,7 +54,12 @@ export class FilledOrdersComponent extends BaseComponent implements OnInit {
       .subscribe(openorders => {
         this.zone.run(() => {
           this.filledorders = openorders
-            .filter(o => o.status === OrderStates.Finished || o.status === OrderStates.Canceled)
+            .filter(o => o.status === OrderStates.Finished || 
+                        o.status === OrderStates.Canceled || 
+                        o.status === OrderStates.Expired || 
+                        o.status === OrderStates.Offline || 
+                        o.status === OrderStates.Invalid || 
+                        o.status === OrderStates.RolledBack)
             .map((o) => {
               o['row_class'] = o.side;
               return o;
--- src/app/openorder.ts
+++ src/app/openorder.ts
@@ -1,4 +1,5 @@
 import * as moment from 'moment';
+import * as OrderStates from '../orderstates';
 
 export class Openorder {
   id: string;
@@ -23,6 +24,38 @@ export class Openorder {
 
   public formattedDate() {
     return moment(this.created_at).format('MMM DD HH:mm');
+  }
+
+  public getStatus() {
+    var orderStatus = this.status;
+    var simpleStatus = orderStatus;
+    simpleStatus = (orderStatus == OrderStates.New)             ? 'Creating order'              : simpleStatus;
+    simpleStatus = (orderStatus == OrderStates.Open)            ? 'Open'                        : simpleStatus;
+    simpleStatus = (orderStatus == OrderStates.Accepting)       ? 'Exchange in progress (1/5)'  : simpleStatus;
+    simpleStatus = (orderStatus == OrderStates.Hold)            ? 'Exchange in progress (2/5)'  : simpleStatus;
+    simpleStatus = (orderStatus == OrderStates.Initialized)     ? 'Exchange in progress (3/5)'  : simpleStatus;
+    simpleStatus = (orderStatus == OrderStates.Created)         ? 'Exchange in progress (4/5)'  : simpleStatus;
+    simpleStatus = (orderStatus == OrderStates.Committed)       ? 'Exchange in progress (5/5)'  : simpleStatus;
+    simpleStatus = (orderStatus == OrderStates.Finished)        ? 'Complete'                    : simpleStatus;
+    simpleStatus = (orderStatus == OrderStates.Expired)         ? 'Expired'                     : simpleStatus;
+    simpleStatus = (orderStatus == OrderStates.Offline)         ? 'Offline'                     : simpleStatus;
+    simpleStatus = (orderStatus == OrderStates.Invalid)         ? 'Invalid'                     : simpleStatus;
+    simpleStatus = (orderStatus == OrderStates.RolledBack)      ? 'Order failed, refunded'      : simpleStatus;
+    simpleStatus = (orderStatus == OrderStates.RollbackFailed)  ? 'Order failed, not refunded'  : simpleStatus;
+    simpleStatus = (orderStatus == OrderStates.Canceled)        ? 'Cancelled'                   : simpleStatus;
+    return simpleStatus;
+  }
+  
+  public datetimeFormat() {
+    //datetime format: 2019-01-18T21:18:05.005537Z
+    var datetime = this.created_at;
+    if (moment(new Date()).format('MMM DD')==moment(datetime).format('MMM DD')) {
+      // if today, show hr:min:s:ms format
+      return moment(datetime).format('HH:mm:ss');
+    } else {
+      // if not today, show month-day-hr:min format
+      return moment(datetime).format('MMM DD HH:mm');
+    }
   }
 
   static createOpenOrder(props: any): Openorder {
