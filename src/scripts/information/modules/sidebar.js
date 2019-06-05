53e465be0cf35b766bfc7e0b8e064ad63d12c97a
hanniabu <hanniabu@gmail.com>
2019-06-05 13:36:06 -0400

    rename vaiable for sidebar menu items

--- src/scripts/information/modules/sidebar.js
+++ src/scripts/information/modules/sidebar.js
@@ -3,8 +3,8 @@ const renderSidebar = ({ state }) => {
   const header = `<div class=sidebar-header>Menu</div>`;
   const links = state.get('sidebarItems')
     .map((item, idx) => {
-      const { text = '' } = item;
-      return `<div class="sidebar-item js-sidebarItem ${active === idx ? 'sidebar-active-link' : 'sidebar-inactive-link'}" data-sidebar-index="${idx}" > ${text}</div>`;
+      const { sidebarText = '' } = item;
+      return `<div class="sidebar-item js-sidebarItem ${active === idx ? 'sidebar-active-link' : 'sidebar-inactive-link'}" data-sidebar-index="${idx}" > ${sidebarText}</div>`;
     })
     .join('\n');
   const html = header + links;
