f0d9886a09ece7e81b3e592d743a6cea7fa555ff
Cindy* <adamashton11@gmail.com>
2019-07-26 14:04:40 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-window.electron.ipcRenderer.setMaxListeners(0);
+window.require.electron.ipcRenderer.setMaxListeners(0);
 
 String.prototype['capitalize'] = function() {
   return this.charAt(0).toUpperCase() + this.slice(1);
@@ -19,7 +19,7 @@ platformBrowserDynamic().bootstrapModule(AppModule)
 
 let alertTimeout;
 let count = 0;
-window.electron.ipcRenderer.on('error', (e, { name, message }) => {
+window.require.electron.ipcRenderer.on('error', (e, { name, message }) => {
   if(count === 0) {
     count++;
     alert(name + ': ' + message);
@@ -31,14 +31,14 @@ window.electron.ipcRenderer.on('error', (e, { name, message }) => {
     alert(name + ': ' + message);
   }
   if (name === 'Unsupported Version')
-    window.electron.ipcRenderer.send('quitResetFirstRun');
+    window.require.electron.ipcRenderer.send('quitResetFirstRun');
 });
 
-window.document.addEventListener('drop', e => {
+window.require.document.addEventListener('drop', e => {
   e.preventDefault();
   e.stopPropagation();
 });
-window.document.addEventListener('dragover', e => {
+window.require.document.addEventListener('dragover', e => {
   e.preventDefault();
   e.stopPropagation();
 });
