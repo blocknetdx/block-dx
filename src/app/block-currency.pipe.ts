b2ce0081ea8e9ab3ed63a9925e7ffec082cfd70f
rBurgett <ryan@burgettweb.net>
2019-08-07 18:11:31 -0400

    Added 8 digit tooltip for rounded BTC prices
    Removed html markup from BTC price tooltips
    
    Ignore remaining zeroes on tooltip numbers

--- src/app/block-currency.pipe.ts
+++ src/app/block-currency.pipe.ts
@@ -8,28 +8,13 @@ export class BlockCurrencyPipe implements PipeTransform {
 
   constructor(private numberFormatPipe: NumberFormatPipe) {}
 
-  transform(value: any, symbol: any, decimalMax, removeRemainingZeroes = false): any {
+  transform(value: any, symbol: any, decimalMax, ignoreRemainingZeroes = false): any {
     const format = symbol !== 'USD' ? `1.${decimalMax}-${decimalMax}` : '1.2-2';
     let v = this.numberFormatPipe.transform(value,format);
 
-    if(v && !removeRemainingZeroes) {
+    if(v && !ignoreRemainingZeroes) {
       // Add remaining zeroes afte the decimal into separate span for display purposes
       v = v.replace(/(0{2,})$/, '<span>$1</span>');
-    } else if(v && /\./.test(v) && removeRemainingZeroes) {
-      // Remove remaining zeroes after the decimal
-      const final = v.length - 1;
-      let begin;
-      for(let i = final; i--; i > 0) {
-        const char = v[i];
-        if(char === '.') {
-          begin = i + 2;
-          break;
-        } else if(char !== '0') {
-          begin = i + 1;
-          break;
-        }
-      }
-      v = v.slice(0, begin);
     }
     return v;
   }
