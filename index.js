c3279a3fd81f238c0552c06ff84a12690ab3b2a8
hanniabu <hanniabu@gmail.com>
2020-03-23 13:11:56 -0400

    Update Blocknet wallet min version

--- index.js
+++ index.js
@@ -1892,7 +1892,7 @@ function isTokenPairValid(keyPair) {
 
 // check for version number. Minimum supported blocknet client version
 function versionCheck(version) {
-  if (version < 4000100) {
+  if (version < 4010000) {
     return {name: Localize.text('Unsupported Version', 'universal'), message: Localize.text('Block DX requires Blocknet wallet version 4.0.0 or greater.', 'universal')};
   }
   return null;
